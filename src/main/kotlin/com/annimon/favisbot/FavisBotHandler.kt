8c2f1c0c9c0b40ef1bef1928a6ca84a5b6354229
Victor <annimon119@gmail.com>
2020-07-06 00:59:37 +0300

    Add /help command

--- src/main/kotlin/com/annimon/favisbot/FavisBotHandler.kt
+++ src/main/kotlin/com/annimon/favisbot/FavisBotHandler.kt
@@ -53,6 +53,7 @@ class FavisBotHandler(
             when (command.toLowerCase()) {
                 "/start" -> cmdStart(message)
                 "/register" -> cmdRegister(message)
+                "/help" -> cmdHelp(message)
                 else -> log.info("Unknown command: $command")
             }
             return null
@@ -249,6 +250,43 @@ class FavisBotHandler(
                         .callAsync(this)
             }
         }
+    }
+
+    private fun cmdHelp(message: Message) {
+        log.info("cmdHelp: by ${message.from.id}")
+        val bot = "@${appConfig.botUsername}"
+        val text = """
+            *Usage*
+
+            You need permission to use this bot. To request permission, send /register.
+            
+            Once you get access, you can fully use the bot: add sticker packs, animations, photos, and videos to your collection.
+            To add a sticker pack, just send one sticker from the set.
+            
+            After that, you can log in to the web-form to tag stickers and media from your collection.
+            Send /register to generate or regenerate a unique link to web-form.
+
+
+            *Web-form*
+
+            In the web-form you can choose sticker set or media type and start tagging items with comma-separated values.
+            To delete an item, just clear tags and press Apply.
+            
+            Your tagged stickers and media will be available in inline mode.
+            
+            
+            *Inline mode*
+            
+            Type `$bot` in any chat to access your tagged collection.
+            Type `$bot term` to show items containing the substring "term" tag/
+            For exact match only, use `.` at the end of your query:  `$bot term.`
+            To show all your tagged collection you can use `.all` query.
+            """.trimIndent()
+        Methods.sendMessage()
+                .setChatId(message.from.id.toLong())
+                .setText(text)
+                .enableMarkdown()
+                .callAsync(this)
     }
 
     private fun processMedia(message: Message) {
