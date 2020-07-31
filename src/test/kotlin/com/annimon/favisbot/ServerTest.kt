2a9ef247778d7983deabb2ba3745259722f8ece8
Victor <annimon119@gmail.com>
2020-08-01 01:49:16 +0300

    Fix possible port in use error in tests

--- src/test/kotlin/com/annimon/favisbot/ServerTest.kt
+++ src/test/kotlin/com/annimon/favisbot/ServerTest.kt
@@ -2,31 +2,44 @@ package com.annimon.favisbot
 
 import com.annimon.favisbot.db.*
 import com.google.inject.Guice
-import io.mockk.every
-import io.mockk.mockkObject
-import io.mockk.unmockkAll
+import io.mockk.*
 import kong.unirest.Unirest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Test
 
 class ServerTest {
 
-    private val injector = Guice.createInjector(TestModule())
+    companion object {
+        private val injector = Guice.createInjector(TestModule())
+        private const val baseUrl = "http://127.0.0.1:9377"
+        private val server = injector.getInstance(Server::class.java)
+
+        @BeforeClass
+        @JvmStatic
+        fun setUp() {
+            server.start()
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun tearDown() {
+            server.stop()
+        }
+    }
+
     private val usersRepository = injector.getInstance(UsersRepository::class.java)
     private val userSetsRepository = injector.getInstance(UserSetsRepository::class.java)
-    private val server = injector.getInstance(Server::class.java)
-    private val baseUrl = "http://127.0.0.1:9377"
 
     @Test
     fun `GET meta default appName and user`() {
         val guid = "abc1230f-abcd"
-        server.start()
         val response = Unirest.get("$baseUrl/meta/$guid").asObject(HashMap::class.java)
 
         assertThat(response.status).isEqualTo(200)
         assertThat(response.body["appName"]).isEqualTo("appName")
         assertThat(response.body["user"]).isEqualTo("")
-        server.stop()
     }
 
     @Test
@@ -35,7 +48,6 @@ class ServerTest {
         val user = createDefaultUser(guid)
         mockkObject(usersRepository)
         every { usersRepository.findUserByGUID(guid) } returns user
-        server.start()
         val response = Unirest.get("$baseUrl/meta/$guid").asObject(HashMap::class.java)
 
         assertThat(response.status).isEqualTo(200)
@@ -43,7 +55,6 @@ class ServerTest {
         assertThat(response.body["user"]).isEqualTo("name")
         assertThat(response.body["stickerSets"] as List<*>)
             .contains("!animation", "!document", "!gif", "!photo", "!video")
-        server.stop()
         unmockkAll()
     }
 
@@ -56,21 +67,17 @@ class ServerTest {
         every {
             userSetsRepository.findAllUserSets(user.id)
         } returns listOf("set1", "set2")
-        server.start()
         val response = Unirest.get("$baseUrl/meta/$guid").asObject(HashMap::class.java)
 
         assertThat(response.status).isEqualTo(200)
         assertThat(response.body["stickerSets"] as List<*>).contains("set1", "set2")
-        server.stop()
         unmockkAll()
     }
 
     @Test
     fun `GET items in set empty guid`() {
-        server.start()
         val response = Unirest.get("$baseUrl/items/sample-set").asString()
         assertThat(response.status).isEqualTo(401)
-        server.stop()
     }
 
     @Test
@@ -90,14 +97,12 @@ class ServerTest {
         every {
             userSetsRepository.findAllByStickerSet(119, "sample-set")
         } returns listOf(el1, el2)
-        server.start()
         val response = Unirest.get("$baseUrl/items/sample-set")
             .header("guid", guid)
             .asObject(Array<DbItemWithTag>::class.java)
 
         assertThat(response.status).isEqualTo(200)
         assertThat(response.body).contains(el1, el2)
-        server.stop()
         unmockkAll()
     }
 
